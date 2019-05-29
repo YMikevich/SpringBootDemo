4de0e6634bbdf7671d6736f2a72059d442c4b04f
Yauheni Mikevich <z.mikevich@icloud.com>
2019-05-29 13:57:54 +0200

    Remember-me feature implemented. -data.sql added to create token table if it's not existing; -SecurityConfig.java modified to add tokens storage functionality;

--- src/main/java/by/mikevich/security/config/SecurityConfig.java
+++ src/main/java/by/mikevich/security/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package by.mikevich.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +10,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -15,28 +21,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
+    @Qualifier("dataSource")
+    public DataSource dataSource;
 
     @Autowired
-    public SecurityConfig(final UserDetailsService userDetailsService, final PasswordEncoder passwordEncoder) {
+    public SecurityConfig(final UserDetailsService userDetailsService, final PasswordEncoder passwordEncoder,
+                          final DataSource dataSource) {
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
+        this.dataSource = dataSource;
     }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/users**").authenticated()
+                    .antMatchers("/users**").hasAuthority("ADMIN")
                     .antMatchers("/signUp**").permitAll()
                     .antMatchers("/css/**").permitAll()
-                .and()
+                    .and()
                 .formLogin()
                     .loginPage("/login")
                     .usernameParameter("login")
                     .defaultSuccessUrl("/")
-                    .permitAll();
+                    .permitAll()
+                    .and()
+                .rememberMe()
+                    .rememberMeParameter("remember-me")
+                    .tokenRepository(persistentTokenRepository());
 
         http.csrf().disable();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+
+        return jdbcTokenRepository;
     }
 
     @Override
